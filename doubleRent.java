db55b77e4f616229379104bea3da19929a3b2a3c
kshitija-mande <146907530+kshitija-mande@users.noreply.github.com>
2024-05-02 21:54:16 +0530

    Create doubleRent.java

---
+++ doubleRent.java
@@ -0,0 +1,25 @@
+/*Chefina decided to move into Chef's apartment.
+Chef was initially paying a rent of 
+𝑋
+X rupees. Since Chefina is moving in, the owner decided to double the rent.
+
+Find the final rent Chef needs to pay.*/
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int x=sc.nextInt();
+		int ans = 2*x;
+		System.out.println(ans);
+
+	}
+}
