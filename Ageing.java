9585cf793c8dfeb0b23e22f4f542d2d03ed0a2c1
kshitija-mande <146907530+kshitija-mande@users.noreply.github.com>
2024-05-27 23:33:42 +0530

    Create Ageing.java

---
+++ Ageing.java
@@ -0,0 +1,34 @@
+/*Ageing
+Chef's current age is 
+20
+20 years, while Chefina's current age is 
+10
+10 years.
+Determine Chefina's age when Chef will be 
+𝑋
+X years old.
+
+Note: Assume that Chef and Chefina were born on same day and same month (just different year).*/
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc= new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+	   	int x= sc.nextInt();
+		int ans = x-10;
+		System.out.println(ans);
+
+	}
+
+	}
+}
